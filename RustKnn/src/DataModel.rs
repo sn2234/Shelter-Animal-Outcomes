4644f98f097de0677173f3b04d9e79a8fb8b4dd6
nobody <nobody@nowhere.com>
2016-05-09 17:38:24 +0300

    Minkowsky and manhattan distance measurements added

--- RustKnn/src/DataModel.rs
+++ RustKnn/src/DataModel.rs
@@ -33,16 +33,46 @@ where T: Mul<Output=T> + Copy {
     x*x
 }
 
-impl Knn for Animal {
-    fn distance(&self, y : &Self) -> f64 {
-        let sumSquares = sq(self.Name - y.Name) +
-                         sq(self.DateTime - y.DateTime) +
-                         sq(self.AnimalType - y.AnimalType) +
-                         sq(self.SexuponOutcome - y.SexuponOutcome) +
-                         sq(self.AgeuponOutcome - y.AgeuponOutcome) +
-                         sq(self.Breed - y.Breed) +
-                         sq(self.Color - y.Color);
+fn euclidianDistance(from: &Animal, to: &Animal) -> f64 {
+        let sumSquares = sq(from.Name - to.Name) +
+                         sq(from.DateTime - to.DateTime) +
+                         sq(from.AnimalType - to.AnimalType) +
+                         sq(from.SexuponOutcome - to.SexuponOutcome) +
+                         sq(from.AgeuponOutcome - to.AgeuponOutcome) +
+                         sq(from.Breed - to.Breed) +
+                         sq(from.Color - to.Color);
 
         return (sumSquares as f64).sqrt();
+}
+
+fn manhattanDistance(from: &Animal, to: &Animal) -> f64 {
+    let dist =  (from.Name - to.Name).abs() +
+                (from.DateTime - to.DateTime).abs() +
+                (from.AnimalType - to.AnimalType).abs() +
+                (from.SexuponOutcome - to.SexuponOutcome).abs() +
+                (from.AgeuponOutcome - to.AgeuponOutcome).abs() +
+                (from.Breed - to.Breed).abs() +
+                (from.Color - to.Color).abs();
+    
+    return dist;
+}
+
+fn minkowskyDistance(from: &Animal, to: &Animal) -> f64 {
+    let q = 7.0;
+    
+    let distSum =   (from.Name - to.Name).abs().powf(q) +
+                    (from.DateTime - to.DateTime).abs().powf(q) +
+                    (from.AnimalType - to.AnimalType).abs().powf(q) +
+                    (from.SexuponOutcome - to.SexuponOutcome).abs().powf(q) +
+                    (from.AgeuponOutcome - to.AgeuponOutcome).abs().powf(q) +
+                    (from.Breed - to.Breed).abs().powf(q) +
+                    (from.Color - to.Color).abs().powf(q);
+    
+    return distSum.powf(1.0/q);
+}
+
+impl Knn for Animal {
+    fn distance(&self, y : &Self) -> f64 {
+        minkowskyDistance(self, y)
     }
 }
