65e9c1830629bcf6e56b64bce3e328f96dff7ba9
sn2234 <sn2234@gmail.com>
2016-05-22 20:44:15 +0300

    Learn optimal number of neighbors

--- AnimalsPython/AnimalsPython.py
+++ AnimalsPython/AnimalsPython.py
@@ -3,6 +3,24 @@ import DataModel
 from sklearn import cross_validation
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
+import numpy as np
+
+def findBestNeighborsCount(x_train, x_cv, y_train, y_cv, kFrom, kTo, steps):
+    kVec = np.linspace(kFrom, kTo, steps, dtype=int)
+
+    bestAcc = 0.0
+    bestK = 0
+
+    for k in kVec:
+        knn = neighbors.KNeighborsClassifier(n_neighbors=k, weights='distance', metric='euclidean')
+        knn.fit(x_train, y_train)
+        acc_cv = accuracy_score(y_cv, knn.predict(x_cv))
+        print("Accuracy: {} with {} neighbors".format(acc_cv, k))
+        if acc_cv > bestAcc:
+            bestAcc = acc_cv
+            bestK = k
+
+    return (bestK, bestAcc)
 
 dl = DataModel.DataLoader()
 
@@ -10,10 +28,15 @@ x, y = dl.loadTrainData("..\\train.csv")
 
 (x_train, x_cv, y_train, y_cv) = cross_validation.train_test_split(x, y, test_size=0.2)
 
-knn = neighbors.KNeighborsClassifier(n_neighbors=3)
 
-knn.fit(x_train, y_train)
+(k, acc) = findBestNeighborsCount(x_train, x_cv, y_train, y_cv, 50, 200, 50)
 
-acc_cv = accuracy_score(y_cv, knn.predict(x_cv))
+print("Best k: {} with accuracy: {}".format(k, acc))
 
-print("Accuracy on CV dataset: {}".format(acc_cv))
+#knn = neighbors.KNeighborsClassifier(n_neighbors=3)
+
+#knn.fit(x_train, y_train)
+
+#acc_cv = accuracy_score(y_cv, knn.predict(x_cv))
+
+#print("Accuracy on CV dataset: {}".format(acc_cv))
